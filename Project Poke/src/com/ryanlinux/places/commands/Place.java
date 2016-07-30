56a1186015bdc33eb3e262016e47d740762cc6aa
Wyatt J Herkamp <kinglinuxwh@gmail.com>
2016-07-30 10:17:18 -0500

    Add files via upload

--- Project Poke/src/com/ryanlinux/places/commands/Place.java
+++ Project Poke/src/com/ryanlinux/places/commands/Place.java
@@ -1,26 +1,60 @@
 package com.ryanlinux.places.Commands;
 
+//Definging imports
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.ryanlinux.places.core.Main;
 
-public class Place implements CommandExecutor{
-	
+public class Place implements CommandExecutor {
 	private Main plugin;
-	public Place(Main plugin) {
+	public static String Prefix;
 
-		this.plugin = plugin;
+	public Place(Main pl) {
+
+		plugin = pl;
 	}
 
-	public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
-		Player p = (Player) sender;
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+		Player player = (Player) sender; // Defining the player
 		if (cmd.getName().equalsIgnoreCase("place")) {
 			if (args.length == 0) {
-			
-			}
-			
 
+				player.sendMessage(Main.GetPrefix() + "Arguments you can enter");
+
+				return true;
+			}
+			if (args[0].equalsIgnoreCase("go")) {
+
+			}
+
+			if (args[0].equalsIgnoreCase("restart")) {
+
+			}
+			if (args[0].equalsIgnoreCase("lock")) {
+
+			}
+			if (args[0].equalsIgnoreCase("unlock")) {
+
+			}
+			if (args[0].equalsIgnoreCase("sethome")) {
+
+			}
+			if (args[0].equalsIgnoreCase("ban")) {
+
+			}
+			if (args[0].equalsIgnoreCase("unban")){
+				
+				
+			}
+			else{
+				
+				
+			}
+			return true;
 		}
 
 		return false;
