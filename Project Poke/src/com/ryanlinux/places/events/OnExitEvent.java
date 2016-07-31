a5e196cf9ee5adaefde23b1f3c0b0b9cbbd87624
Wyatt J Herkamp <kinglinuxwh@gmail.com>
2016-07-30 23:44:49 -0500

    Add files via upload

--- Project Poke/src/com/ryanlinux/places/events/OnExitEvent.java
+++ Project Poke/src/com/ryanlinux/places/events/OnExitEvent.java
@@ -1,5 +1,11 @@
 package com.ryanlinux.places.events;
 
+import org.bukkit.event.player.PlayerQuitEvent;
+
 public class OnExitEvent {
+
+	public void ExitEvent(PlayerQuitEvent e) {
+
+	}
 
 }
--- Project Poke/src/com/ryanlinux/places/events/OnJoinEvent.java
+++ Project Poke/src/com/ryanlinux/places/events/OnJoinEvent.java
@@ -1,5 +1,11 @@
 package com.ryanlinux.places.events;
 
+import org.bukkit.event.player.PlayerJoinEvent;
+
 public class OnJoinEvent {
+
+	public void JoinEvent(PlayerJoinEvent e) {
+
+	}
 
 }
