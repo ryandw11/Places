0461508064700fe0aed112f254717491363e4882
Wyatt J Herkamp <kinglinuxwh@gmail.com>
2016-07-30 23:48:37 -0500

    Add files via upload

--- Project Poke/src/com/ryanlinux/places/events/OnJoinEvent.java
+++ Project Poke/src/com/ryanlinux/places/events/OnJoinEvent.java
@@ -1,5 +1,12 @@
 package com.ryanlinux.places.events;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+
 public class OnJoinEvent {
+    @EventHandler
+	public void JoinEvent(PlayerJoinEvent e) {
+
+	}
 
 }
