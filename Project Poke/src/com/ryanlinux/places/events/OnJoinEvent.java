d64e6a0202e6f1e82bbb4098a37422b882f3a06a
ryandw11 <ryandw02@gmail.com>
2016-08-02 19:07:35 -0700

    Update OnJoinEvent.java

--- Project Poke/src/com/ryanlinux/places/events/OnJoinEvent.java
+++ Project Poke/src/com/ryanlinux/places/events/OnJoinEvent.java
@@ -4,8 +4,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class OnJoinEvent {
-    @EventHandler
-	public void JoinEvent(PlayerJoinEvent e) {
+	
+    	@EventHandler
+	public void JoinEvent(PlayerJoinEvent event) {
 
 	}
 
